0b317f74863b023aeb5efd71054da7210a2c7fda
Anwar Anwar <a.ammarr17@gmail.com>
2024-04-29 00:59:29 +0100

    set up boutique v1

---
+++ src/app/components/boutique-card/boutique-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boutique-card',
+  templateUrl: './boutique-card.component.html',
+  styleUrls: ['./boutique-card.component.css']
+})
+export class BoutiqueCardComponent {
+
+}
