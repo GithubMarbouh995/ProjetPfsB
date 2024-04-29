359bcc72ff2de9ab42f064a0160983519350d3da
Anwar Anwar <a.ammarr17@gmail.com>
2024-04-29 14:35:24 +0100

    Boutique Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoutiquePageComponent } from './pages/boutique-page/boutique-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : BoutiquePageComponent},
+  {path : 'boutique', component : BoutiquePageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
