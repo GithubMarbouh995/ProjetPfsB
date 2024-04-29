82def455c366d04127b5fb6be0a9f0b7753242c2
Anwar Anwar <a.ammarr17@gmail.com>
2024-04-29 23:32:28 +0100

    Boutique-detail

---
+++ src/app/pages/boutique-detail-page/boutique-detail-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boutique-detail-page',
+  templateUrl: './boutique-detail-page.component.html',
+  styleUrls: ['./boutique-detail-page.component.css']
+})
+export class BoutiqueDetailPageComponent {
+
+}
