36e9dddcaf6b7d472aaa0d09213fdebaf1322874
unknown <Mohamed Marbouh>
2024-05-13 21:14:44 +0100

    dash-admin

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,54 +6,11 @@ import {Component, ElementRef} from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  title = 'admindashboard';
-
-  constructor(private el:ElementRef){}
-  ngOnInit(): void {
-
-    let alldrpdwn = document.querySelectorAll('.dropdow-container');
-    console.log(alldrpdwn,'alldrpdwn#');
-    alldrpdwn.forEach((item:any)=>{
-      const a = item.parentElement?.querySelector('a:first-child');
-      console.log(a,'a#');
-      a.addEventListener('click',(e:any)=>{
-        e.preventDefault();
-        this.el.nativeElement.classList.toggle('active');
-        item.classList.toggle('show');
-      });
-
-    });
-
-  }
-
-
-  // responsivemenu
-  responsiveMenu:any;
-  // responsivemaincontent
-  responsiveContent:any;
-  defaultStatus=true;
-  openNav(status:any)
+  //Sidebar toggle show hide function
+  status = false;
+  addToggle()
   {
-    if(status===this.defaultStatus)
-    {
-      this.responsiveMenu = {
-        'display':'block'
-      }
-      this.responsiveContent={
-        'margin-left':'150px'
-      }
-      this.defaultStatus = false;
-    }else
-    {
-      this.responsiveMenu = {
-        'display':null
-      }
-      this.responsiveContent={
-        'margin-left':null
-      }
-      this.defaultStatus=true;
-    }
-
+    this.status = !this.status;
   }
 
 }
